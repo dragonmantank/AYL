e4b278284ed3fda70fd6cd39cc460962daaa1d8a
Chris Tankersley <dragonmantank@gmail.com>
2010-12-29 16:34:13 -0500

    Added very basic file upload for CKEditor

--- application/modules/admin/controllers/FileController.php
+++ application/modules/admin/controllers/FileController.php
@@ -5,6 +5,7 @@ class Admin_FileController extends Zend_Controller_Action
     public function browseAction()
     {
         $this->_helper->layout->disableLayout();
+        
         try {
             $files = new DirectoryIterator(APPLICATION_PATH.'/../public/images/uploaded');
         } catch (Exception $e) {
@@ -16,6 +17,22 @@ class Admin_FileController extends Zend_Controller_Action
 
     public function uploadAction()
     {
-        
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+
+        $funcNum = $_GET['CKEditorFuncNum'] ;
+
+        $upload = new Zend_File_Transfer_Adapter_Http();
+        $upload->setDestination(APPLICATION_PATH.'/../public/images/uploaded');
+        $upload->receive();
+
+        try {
+            $url = '/images/uploaded/'.basename($upload->getFileName());
+        } catch(Zend_File_Transfer_Exception $e) {
+            $message = $e->getMessage();
+            $url = '';
+        }
+
+        echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction($funcNum, '$url', '$message');</script>";
     }
 }
